4f551a1b0d4d8a58169ba87e9b8156c5bc5a0947
Ruslan <diego.ruslan.rus@gmail.com>
2021-04-26 22:11:00 +0300

    добовление частичного главного меню
    Добавлено главное меню, прописан скрипт для перехода по уровням, добавлены стили значков

---
+++ Assets/Script/UiManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenLVLMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OpenLVL1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OpenLVL2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void OpenLVL3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void OpenLVL4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+    public void OpenLVL5()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
+    }
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+  
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+
+}
