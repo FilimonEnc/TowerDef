1476671df481ffa60fcac2a56debaad4c06670b1
Ruslan <diego.ruslan.rus@gmail.com>
2021-05-19 22:13:31 +0300

    Добавление спавна, + таймер волны
    в игру добавлен спавн енеми, который происходит через некоторое время, еще добавлен таймер, который будет уменьшаться. он будет означать начало следующей волны, каждую волну спавнится все больше и больше объектов. пока что объекты 1 типа

---
+++ Assets/Script/WaveSpawner.cs
@@ -0,0 +1,47 @@
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 1;
+    public float countdown = 2f;
+    private int waveNumber = 0;
+    public float SecondsSpawn = 0.5f;
+    public Text WaveCountdownText;
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+
+        }
+        countdown -= Time.deltaTime;
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i =0; i< waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(SecondsSpawn);
+        }
+
+        waveNumber++;
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+}
